21f72ab84f68d24ef37b824f218233a67e509653
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:02:13 -0500

    TUnit conversion

--- test/Soenneker.HttpContents.PooledByteArrays.Tests/PooledByteArrayContentTests.cs
+++ test/Soenneker.HttpContents.PooledByteArrays.Tests/PooledByteArrayContentTests.cs
@@ -1,11 +1,10 @@
 using Soenneker.Tests.Unit;
-using Xunit;
 
 namespace Soenneker.HttpContents.PooledByteArrays.Tests;
 
 public sealed class PooledByteArrayContentTests : UnitTest
 {
-    [Fact]
+    [Test]
     public void Default()
     {
 
